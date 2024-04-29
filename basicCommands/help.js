8955c0739fd4aab904b3692cb9335452e2cb396b
shouryajod123 <168352984+shouryajod123@users.noreply.github.com>
2024-04-29 20:16:34 +0530

    help.js

--- basicCommands/help.js
+++ basicCommands/help.js
@@ -17,7 +17,7 @@ module.exports = {
         url: 'https://discord.gg/FUEHs7RCqz'
     })
      
-      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By Flame**\n\n__**COMMANDS :**__ `)
+      .setDescription(`__**STATS :**__\n\n> **📊 Bot in servers:** ${serverCount}\n> **🟢 Bot Ping:** ${botPing}ms\n> **👑 Made By [GlaceYT](https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A)**\n\n__**COMMANDS :**__ `)
       .addFields(
         // Basic commands category
         {
@@ -59,3 +59,24 @@ module.exports = {
       .setThumbnail(botUser.avatarURL({ dynamic: true, format: 'png', size: 1024 }))
       .setImage(`https://cdn.discordapp.com/attachments/1170652474563117138/1224660177039396876/MULTI_MUSIC_v1.5.png?ex=661e4cab&is=660bd7ab&hm=f7a542462fabaf9c1530ece5aa72597cff3ac032876bba46df5ddba7e122ea99&`);
 
+    const button1 = new ButtonBuilder()
+      .setLabel('YouTube')
+      .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+      .setStyle(ButtonStyle.Link);
+
+    const button2 = new ButtonBuilder()
+      .setLabel('Discord')
+      .setURL('https://discord.gg/FUEHs7RCqz')
+      .setStyle(ButtonStyle.Link);
+
+    const button3 = new ButtonBuilder()
+      .setLabel('Code')
+      .setURL('https://replit.com/@BEASTGAMERS1/MULTI-MUSIC-BOT-v200?v=1')
+      .setStyle(ButtonStyle.Link);
+      
+    const row = new ActionRowBuilder()
+      .addComponents(button1, button2, button3);
+    
+    message.reply({ embeds: [embed], components: [row] });
+  },
+};
